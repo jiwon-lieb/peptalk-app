9600acd200ed781e9cac87ee842fa1816a8794f9
Jiwon Kwak <jwkwak1002@gmail.com>
2025-02-13 20:52:46 +0900

    Added new moods

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const getAdviceButton = document.getElementById('getAdvice');
 const adviceMessage = document.getElementById('advice-message');
-const mbtiSelect = document.getElementById('mbti');
+const mbtiSelect = document.getElementById('mbti_type');
 const moodSelect = document.getElementById('mood');
-const API_BASE = 'https://peptalk-api.netlify.app';
+const API_BASE = 'https://api.peptalk.jiwonkwak.co';
 
 getAdviceButton.addEventListener('click', async () => {
   const mbti = mbtiSelect.value;
   const mood = moodSelect.value;
-  const apiUrl = `${API_BASE}/api/peptalk/${mbti}?mood=${mood}`;
+  const apiUrl = `${API_BASE}/api/peptalk/${mbti_type}?mood=${mood}`;
 
   try {
     const response = await fetch(apiUrl);
